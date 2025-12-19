17ce9e483db76fd2c44163406db7f21c0a96d056
miona <sretenovicmiona7@gmail.com>
2025-12-19 21:45:50 +0100

    chore: add clang-format/clang-tidy and format codebase

--- AlgoVision/Headers/MainWindow.h
+++ AlgoVision/Headers/MainWindow.h
@@ -5,12 +5,11 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
--- AlgoVision/Source/MainWindow.cpp
+++ AlgoVision/Source/MainWindow.cpp
@@ -1,14 +1,11 @@
-#include "mainwindow.h"
+#include "MainWindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
