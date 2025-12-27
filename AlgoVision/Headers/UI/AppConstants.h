9dd78ac8b867f66e7b9a95226c72d43d72480924
Bogdan Micic <bogdanmicic03vr@gmail.com>
2025-12-27 16:49:34 +0100

    feat: add new header file for ui element's attribute values

---
+++ AlgoVision/Headers/UI/AppConstants.h
@@ -0,0 +1,12 @@
+#ifndef APPCONSTANT_H
+#define APPCONSTANT_H
+
+#include <qsize.h>
+
+namespace AppConstants {
+    // start page element's attributes values
+    constexpr int btnSize = 120;
+    constexpr int btnSpacing = 24;
+}
+
+#endif // APPCONSTANT
