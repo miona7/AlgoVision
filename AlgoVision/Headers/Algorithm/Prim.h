09df15865e9e8e2ae29e1cba4dfd18466ecee82a
miona <sretenovicmiona7@gmail.com>
2025-12-31 17:55:26 +0100

    feat(Prim): implement algorithm and add tests

---
+++ AlgoVision/Headers/Algorithm/Prim.h
@@ -0,0 +1,23 @@
+#ifndef PRIM_H
+#define PRIM_H
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <limits>
+
+#include "Algorithm.h"
+#include "BFS.h"
+
+class Prim : public Algorithm {
+public:
+    explicit Prim(const std::shared_ptr<Graph>&);
+
+    void checkConditions() const;
+    void execute(unsigned idStartNode = 0, unsigned idEndNode = 0) override;
+
+private:
+    void prim();
+};
+
+#endif // PRIM_H
---
+++ AlgoVision/Tests/Algorithm/primTest.cpp
@@ -0,0 +1,60 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+
+#include "Prim.h"
+#include "WeightedUndirectedGraph.h"
+#include "WeightedDirectedGraph.h"
+
+TEST_CASE("Prim works on connected undirected weighted graph", "[PRIM]") {
+    auto g = std::make_shared<WeightedUndirectedGraph>();
+
+    g->addNode(1);
+    g->addNode(2);
+    g->addNode(3);
+    g->addNode(4);
+
+    g->addEdge(1, 2, 1);
+    g->addEdge(2, 3, 2);
+    g->addEdge(3, 4, 3);
+    g->addEdge(1, 4, 10);
+
+    Prim prim(g);
+
+    REQUIRE_NOTHROW(prim.execute());
+}
+
+TEST_CASE("Prim throws on disconnected graph", "[PRIM]") {
+    auto g = std::make_shared<WeightedUndirectedGraph>();
+
+    g->addNode(1);
+    g->addNode(2);
+    g->addNode(3);
+
+    g->addEdge(1, 2, 1);
+    // cvor 3 je izolovan
+
+    Prim prim(g);
+
+    REQUIRE_THROWS_AS(prim.execute(), std::runtime_error);
+}
+
+TEST_CASE("Prim throws on directed graph", "[PRIM]") {
+    auto g = std::make_shared<WeightedDirectedGraph>();
+
+    g->addNode(1);
+    g->addNode(2);
+
+    g->addEdge(1, 2, 5);
+
+    Prim prim(g);
+
+    REQUIRE_THROWS_AS(prim.execute(), std::runtime_error);
+}
+
+TEST_CASE("Prim throws on empty graph", "[PRIM]") {
+    auto g = std::make_shared<WeightedUndirectedGraph>();
+
+    Prim prim(g);
+
+    REQUIRE_THROWS_AS(prim.execute(), std::runtime_error);
+}
