5d1ce0d8b11327fbafc7bee05a2f16fd1898f8f7
Bogdan Micic <bogdanmicic03vr@gmail.com>
2026-01-05 16:54:22 +0100

    feat: implement add node functionality

--- AlgoVision/Headers/View/GraphScene.h
+++ AlgoVision/Headers/View/GraphScene.h
@@ -5,7 +5,14 @@
 
 
 class GraphScene : public QGraphicsScene {
+public:
+    explicit GraphScene(QObject *parent = nullptr);
 
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+
+private:
+    void addNode(QPointF position);
 };
 
 #endif // GRAPHSCENE_H
--- AlgoVision/Source/View/GraphScene.cpp
+++ AlgoVision/Source/View/GraphScene.cpp
@@ -1,1 +1,29 @@
 #include "GraphScene.h"
+#include "Node.h"
+#include "NodeItem.h"
+#include <QGraphicsSceneMouseEvent>
+
+
+GraphScene::GraphScene(QObject *parent)
+    : QGraphicsScene(parent) {
+    setSceneRect(0, 0, 3000, 3000);
+}
+
+void GraphScene::mousePressEvent(QGraphicsSceneMouseEvent *event) {
+    const auto clickPos = event->scenePos();
+    QGraphicsItem* item = itemAt(clickPos, QTransform());
+
+    if (!item) {
+        addNode(clickPos);
+        event->accept();
+        return;
+    }
+
+    QGraphicsScene::mousePressEvent(event);
+}
+
+void GraphScene::addNode(QPointF position) {
+    Node* nodeModel = new Node(1, position.x(), position.y());
+    NodeItem* nodeItem = new NodeItem(nodeModel);
+    addItem(nodeItem);
+}
