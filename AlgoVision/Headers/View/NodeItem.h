49cec6007c1c396941f2cd5c91709fbe042b03fb
Bogdan Micic <bogdanmicic03vr@gmail.com>
2026-01-05 13:09:17 +0100

    feat: add node item class for node drawing

--- AlgoVision/Headers/View/NodeItem.h
+++ AlgoVision/Headers/View/NodeItem.h
@@ -2,11 +2,27 @@
 #define NODEITEM_H
 
 #include <QGraphicsObject>
-
+#include "Node.h"
+#include "AppConstants.h"
 
 class NodeItem : public QGraphicsObject {
     Q_OBJECT
 
+public:
+    explicit NodeItem(Node* model);
+
+    Node* modelNode() const;
+    void setModelNode(Node *newModel);
+
+protected:
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+private:
+    Node* m_modelNode;
+    qreal m_radius{AppConstants::defaultRadius};
+    qreal m_borderWidth{AppConstants::defaultBorderWidth};
 };
 
 #endif // NODEITEM_H
--- AlgoVision/Source/View/NodeItem.cpp
+++ AlgoVision/Source/View/NodeItem.cpp
@@ -1,1 +1,40 @@
 #include "NodeItem.h"
+#include <QPen>
+#include <QPainter>
+
+NodeItem::NodeItem(Node* modelNode)
+    : m_modelNode(modelNode) {
+    setFlags(ItemIsMovable | ItemIsSelectable);
+    setZValue(-1);
+    setPos(modelNode->getPosition().first, modelNode->getPosition().second);
+}
+
+Node *NodeItem::modelNode() const {
+    return m_modelNode;
+}
+
+void NodeItem::setModelNode(Node *newModelNode) {
+    m_modelNode = newModelNode;
+}
+
+QRectF NodeItem::boundingRect() const {
+    return QRectF(-m_radius - m_borderWidth,
+                  -m_radius - m_borderWidth,
+                  2 * (m_radius + m_borderWidth),
+                  2 * (m_radius + m_borderWidth));
+}
+
+QPainterPath NodeItem::shape() const {
+    QPainterPath path;
+    path.addEllipse(-m_radius, -m_radius, 2 * m_radius, 2 * m_radius);
+    return path;
+}
+
+void NodeItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    QPen pen(Qt::black, m_borderWidth);
+    QBrush brush(Qt::green);
+
+    painter->setPen(pen);
+    painter->setBrush(brush);
+    painter->drawEllipse(QRectF(-m_radius, -m_radius, 2 * m_radius, 2 * m_radius));
+}
