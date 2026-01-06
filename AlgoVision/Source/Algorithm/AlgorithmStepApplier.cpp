31f5327c7bff4ef609aeefa82e211462d3b7b269
miona <sretenovicmiona7@gmail.com>
2026-01-06 14:12:05 +0100

    feat: add AlgorithmStepApplier class for applying AlgorithmSteps

---
+++ AlgoVision/Source/Algorithm/AlgorithmStepApplier.cpp
@@ -0,0 +1,8 @@
+#include "AlgorithmStepApplier.h"
+
+AlgorithmStepApplier::AlgorithmStepApplier(const std::shared_ptr<Graph>& g) : m_graph(g) {
+}
+
+void AlgorithmStepApplier::apply(const AlgorithmStep& step) {
+    // TODO
+}
