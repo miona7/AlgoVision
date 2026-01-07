f4a2646942102b4aa77fc3eb4c1e6b8863239b67
Bogdan Micic <bogdanmicic03vr@gmail.com>
2026-01-07 15:57:30 +0100

    feat: add directed edge item(view) class

---
+++ AlgoVision/Headers/View/DirectedEdgeItem.h
@@ -0,0 +1,25 @@
+#ifndef DIRECTED_EDGEITEM_H
+#define DIRECTED_EDGEITEM_H
+
+#include <EdgeItem.h>
+
+class DirectedEdgeItem : public EdgeItem {
+
+public:
+    DirectedEdgeItem(Edge *modelEdge, NodeItem *sourceNode, NodeItem *destNode);
+
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+    QPainterPath edgePath() const override;
+
+private:
+    qreal m_skewness{50.0};
+    qreal m_arrowSize{10.0};
+
+    QPainterPath arrowPath() const;
+    QPointF calculateNormal() const;
+};
+
+#endif // DIRECTED_EDGEITEM
---
+++ AlgoVision/Source/View/DirectedEdgeItem.cpp
@@ -0,0 +1,46 @@
+#include <DirectedEdgeItem.h>
+#include <QPainter>
+#include <QPen>
+
+
+DirectedEdgeItem::DirectedEdgeItem(Edge *modelEdge, NodeItem *sourceNode, NodeItem *destNode)
+    : EdgeItem(modelEdge, sourceNode, destNode) {}
+
+QRectF DirectedEdgeItem::boundingRect() const {
+    qreal offset = m_penWidth;
+    return edgePath().boundingRect().adjusted(-offset, -offset, offset, offset);
+}
+
+QPainterPath DirectedEdgeItem::shape() const {
+    QPainterPathStroker stroker;
+    qreal offset = m_penWidth + 3.0; // bigger offset, so the click would be easier, more UX friendly
+    stroker.setWidth(offset);
+    return stroker.createStroke(edgePath());
+}
+
+void DirectedEdgeItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    QPen pen(Qt::black, m_penWidth);
+    painter->setPen(pen);
+    painter->drawPath(edgePath());
+}
+
+QPainterPath DirectedEdgeItem::edgePath() const {
+    QPainterPath path;
+    path.moveTo(m_sourcePoint);
+    QPointF normal = calculateNormal();
+    QPointF middle = (m_sourcePoint + m_destPoint) / 2.0 - normal * m_skewness;
+    path.quadTo(middle, m_destPoint);
+    return path;
+}
+
+QPainterPath DirectedEdgeItem::arrowPath() const {
+    //TODO: implement arrow
+    return QPainterPath();
+}
+
+QPointF DirectedEdgeItem::calculateNormal() const {
+    QPointF line(m_destPoint.x() - m_sourcePoint.x(), m_destPoint.y() - m_sourcePoint.x());
+    QPointF normal(-line.y(), line.x());
+    normal /= std::hypot(normal.x(), normal.y());
+    return normal;
+}
