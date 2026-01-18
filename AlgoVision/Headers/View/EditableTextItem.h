678110d413d8a2c5bcd5e318fa2e7f5829e59d09
Bogdan Micic <bogdanmicic03vr@gmail.com>
2026-01-18 17:22:23 +0100

    chore: add EditableTextItem class from feature-graph-drawing branch

---
+++ AlgoVision/Headers/View/EditableTextItem.h
@@ -0,0 +1,32 @@
+#ifndef EDITABLE_TEXTITEM_H
+#define EDITABLE_TEXTITEM_H
+
+#include <QGraphicsTextItem>
+
+class EditableTextItem : public QGraphicsTextItem {
+    Q_OBJECT
+
+public:
+    explicit EditableTextItem(QGraphicsItem* parent = nullptr);
+
+    QString oldText() const;
+    void    setOldText(const QString& newOldText);
+
+    void startEditing();
+    void finishEditing(bool commit);
+    void centerText();
+
+protected:
+    void keyPressEvent(QKeyEvent* event) override;
+
+signals:
+    void textCommited(const QString& text);
+
+    // notify graphscene if it's EDIT state or not (then it's ADD state)
+    void setEditGraphSceneState(bool edit, EditableTextItem* label = nullptr);
+
+private:
+    QString m_oldText;
+};
+
+#endif
---
+++ AlgoVision/Source/View/EditableTextItem.cpp
@@ -0,0 +1,67 @@
+#include <EditableTextItem.h>
+#include <QKeyEvent>
+#include <QTextCursor>
+
+EditableTextItem::EditableTextItem(QGraphicsItem* parent) : QGraphicsTextItem(parent) {
+    QTextOption opt;
+    opt.setAlignment(Qt::AlignCenter);
+    opt.setWrapMode(QTextOption::WrapAtWordBoundaryOrAnywhere);
+    document()->setDefaultTextOption(opt);
+}
+
+QString EditableTextItem::oldText() const {
+    return m_oldText;
+}
+
+void EditableTextItem::setOldText(const QString& newOldText) {
+    m_oldText = newOldText;
+}
+
+void EditableTextItem::startEditing() {
+    m_oldText = toPlainText();
+    setTextInteractionFlags(Qt::TextEditorInteraction);
+    setFocus();
+
+    QTextCursor c = textCursor();
+    c.select(QTextCursor::Document);
+    setTextCursor(c);
+
+    emit setEditGraphSceneState(true, this);
+}
+
+void EditableTextItem::keyPressEvent(QKeyEvent* event) {
+    if(event->key() == Qt::Key_Return || event->key() == Qt::Key_Enter) {
+        finishEditing(true);
+        return;
+    }
+
+    if(event->key() == Qt::Key_Escape) {
+        finishEditing(false);
+        return;
+    }
+
+    QGraphicsTextItem::keyPressEvent(event);
+}
+
+void EditableTextItem::finishEditing(bool commit) {
+    QTextCursor c = textCursor();
+    c.clearSelection();
+    setTextCursor(c);
+
+    setTextInteractionFlags(Qt::NoTextInteraction);
+    clearFocus();
+
+    if(!commit) {
+        setPlainText(m_oldText);
+    } else {
+        emit textCommited(toPlainText());
+    }
+
+    emit setEditGraphSceneState(false);
+    centerText();
+}
+
+void EditableTextItem::centerText() {
+    QRectF r = boundingRect();
+    setPos(-r.width() / 2, -r.height() / 2);
+}
