1792e13789b2e7d831c8a3ccae13764f8bbd87ad
SofijaJanevska <sofijajan.official@gmail.com>
2026-02-01 01:01:26 +0100

    refactor(AStar): adapt .h and .cpp files to AlgorithmError model by replacing exceptions with return statements, and replacing empty return statements with return std::nullopt

--- AlgoVision/Headers/Algorithm/AStar.h
+++ AlgoVision/Headers/Algorithm/AStar.h
@@ -8,21 +8,23 @@
 #include <memory>
 #include <queue>
 #include <vector>
+#include <optional>
 
 #include "Algorithm.h"
+#include "AlgorithmError.h"
 
 class AStar : public Algorithm {
 public:
     explicit AStar(const std::shared_ptr<Graph>);
 
-    void checkConditions(unsigned, unsigned) const;
-    void execute(unsigned, unsigned) override;
+    std::optional<AlgorithmError> checkConditions(unsigned, unsigned) const;
+    std::optional<AlgorithmError> execute(unsigned, unsigned) override;
 
     const std::vector<unsigned>& getPath() const;
     int                          getTotalCost() const;
 
 private:
-    void aStar(unsigned, unsigned);
+    std::optional<AlgorithmError> aStar(unsigned, unsigned);
     int  heuristic(unsigned, unsigned) const;
 
     std::vector<unsigned> m_path;
