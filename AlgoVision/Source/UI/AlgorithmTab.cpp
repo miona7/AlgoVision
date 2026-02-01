ef03a1ff8376cfdfa5a596a0e5de41fd3ec87d8d
miona <sretenovicmiona7@gmail.com>
2026-02-01 06:35:50 +0100

    fix(UI): add missing node state for Tarjan in legend

--- AlgoVision/Source/UI/AlgorithmTab.cpp
+++ AlgoVision/Source/UI/AlgorithmTab.cpp
@@ -308,8 +308,8 @@ void AlgorithmTab::updateLegendForAlgorithm(const QString& name) {
     m_legendLayout->addWidget(makeLegendItem(Qt::yellow, "Active"));
     m_legendLayout->addWidget(makeLegendItem(Qt::blue, "Visited"));
 
-    if(name == "Dijkstra" || name == "Bellman-Ford" || name == "A*" || name == "Floyd-Warshall" ||
-       name == "Prim") {
+    if(name == "Dijkstra" || name == "Bellman-Ford" || name == "A* (Euclidean heuristic)" || name == "Floyd-Warshall" ||
+       name == "Prim", name == Tarjan) {
         m_legendLayout->addWidget(makeLegendItem(Qt::darkMagenta, "Distance updated"));
     }
 
