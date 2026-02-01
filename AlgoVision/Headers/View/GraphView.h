4ae45abd9e72655ec942d8d609a934ea3e47fa0a
Bogdan Micic <bogdanmicic03vr@gmail.com>
2026-02-01 19:09:16 +0100

    feat: add state mechanism to the GraphView and control to mouse press,move and realease events

--- AlgoVision/Headers/View/GraphView.h
+++ AlgoVision/Headers/View/GraphView.h
@@ -6,10 +6,24 @@
 
 class GraphView : public QGraphicsView {
 public:
+    enum class State {
+        IDLE
+    };
+
     explicit GraphView(QWidget* = nullptr);
     explicit GraphView(QGraphicsScene*, QWidget* = nullptr);
 
+    GraphView::State state() const;
+    void setState(GraphView::State);
+
+protected:
+    void mousePressEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
+    void mouseReleaseEvent(QMouseEvent*) override;
+
 private:
+    GraphView::State m_state {GraphView::State::IDLE};
+
     // inicijalna podesavanja
     void init();
 };
--- AlgoVision/Source/View/GraphView.cpp
+++ AlgoVision/Source/View/GraphView.cpp
@@ -8,9 +8,35 @@ GraphView::GraphView(QGraphicsScene* scene, QWidget* parent) : QGraphicsView(sce
     init();
 }
 
+GraphView::State GraphView::state() const{
+    return m_state;
+}
+
+void GraphView::setState(GraphView::State newState){
+    m_state = newState;
+}
+
+
+void GraphView::mousePressEvent(QMouseEvent* event) {
+    if(m_state == GraphView::State::IDLE) {
+        return;
+    }
+}
+
+void GraphView::mouseMoveEvent(QMouseEvent* event) {
+    if(m_state == GraphView::State::IDLE) {
+        return;
+    }
+}
+
+void GraphView::mouseReleaseEvent(QMouseEvent* event) {
+    if(m_state == GraphView::State::IDLE) {
+        return;
+    }
+}
+
 void GraphView::init() {
     setAlignment(Qt::AlignCenter);
     setTransformationAnchor(QGraphicsView::AnchorViewCenter);
     setResizeAnchor(QGraphicsView::AnchorViewCenter);
-
 }
