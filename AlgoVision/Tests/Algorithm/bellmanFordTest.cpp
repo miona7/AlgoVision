1d022591ab78ad8cb4e43916ae412685a6294c82
miona <sretenovicmiona7@gmail.com>
2026-03-22 19:09:27 +0100

    refactor: move iostream includes from source files to tests files

--- AlgoVision/Tests/Algorithm/bellmanFordTest.cpp
+++ AlgoVision/Tests/Algorithm/bellmanFordTest.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 
+#include <iostream>
+
 #include "BellmanFord.h"
 #include "UnweightedDirectedGraph.h"
 #include "WeightedDirectedGraph.h"
